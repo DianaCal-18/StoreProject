21e96595a6e5585e1cb6501c89b6de62bc3d62ef
DianaCal-18 <148652215+DianaCal-18@users.noreply.github.com>
2024-11-17 17:46:11 -0400

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+/*!
+* Start Bootstrap - Shop Homepage v5.0.6 (https://startbootstrap.com/template/shop-homepage)
+* Copyright 2013-2023 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-shop-homepage/blob/master/LICENSE)
+*/
+// This file is intentionally blank
+// Use this file to add JavaScript to your project
+
+<div class="btn-login">
+    <button 
+        type="button" 
+        onclick="location.href='login.html';" 
+        class="btn btn-primary w-100">
+        Login
+    </button>
+</div>
